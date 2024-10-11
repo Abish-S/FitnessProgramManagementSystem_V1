4617481cc5d5c74044fbab936d758dee30d2a954
Abish-S <ut01325tic2024@gmail.com>
2024-10-11 12:21:50 +0530

    2nd question

---
+++ FitnessProgramManagementSystem/FitnessProgram.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessProgramManagementSystem
+{
+    internal class FitnessProgram
+    {
+        public string FitnessProgramId { get; set; }
+        public string Title { get; set; }
+        public string Duration { get; set; }
+        public string Price { get; set; }
+    }
+}
